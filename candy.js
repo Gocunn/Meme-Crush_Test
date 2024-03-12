b3d82c0d609765d62d137ead8ff22ae6e26b6474
Gocunn <tranhuynhphungquyenagg19@gmail.com>
2024-03-12 18:24:15 +0700

    modified:   candy.js

--- candy.js
+++ candy.js
@@ -1,47 +1,35 @@
+// Danh sách các loại kẹo
 var candies = ["Furina - Red", "Nahida - Or", "Neuvillet - Green", "Raiden - Yellow", "Venti - Blue", "Zhongli - Purple"];
+
+// Ma trận chứa bảng trò chơi
 var board = [];
+
+// Số hàng và cột của bảng trò chơi
 var rows = 9;
 var columns = 9;
-var score = 0;
-var currTile;
-var otherTile;
 
-// Hàm này được gọi khi trang web được tải
+// Hàm được gọi khi trang web được tải
 window.onload = function() {
     startGame(); // Bắt đầu trò chơi
 
     // Lặp lại các hành động trong trò chơi sau mỗi khoảng thời gian nhất định
     window.setInterval(function(){
-        crushCandy();
-        slideCandy();
-        generateCandy();
+        crushCandy(); // Nghiền kẹo
+        slideCandy(); // Trượt kẹo
+        generateCandy(); // Tạo mới kẹo
     }, 100);
 
-    // Xử lý sự kiện theo chuột hoặc cảm ứng tùy theo thiết bị
-    if (isTouchDevice()) {
-        handleTouchEvents(); // Xử lý sự kiện cảm ứng cho điện thoại
-    } else {
-        handleMouseEvents(); // Xử lý sự kiện click cho máy tính
-    }
+    handleEvents(); // Xử lý sự kiện cho cảm ứng và click
 }
 
-// Hàm này được gọi khi bắt đầu trò chơi
+// Hàm này tạo bảng trò chơi
 function startGame() {
-    // Tạo ma trận ô vuông và gán hình ảnh kẹo cho từng ô
     for (let r = 0; r < rows; r++) {
         let row = [];
         for (let c = 0; c < columns; c++) {
             let tile = document.createElement("img");
             tile.id = r.toString() + "-" + c.toString();
             tile.src = "./Image/" + randomCandy() + ".png";
-
-            tile.addEventListener("dragstart", dragStart);
-            tile.addEventListener("dragover", dragOver);
-            tile.addEventListener("dragenter", dragEnter);
-            tile.addEventListener("dragleave", dragLeave);
-            tile.addEventListener("drop", dragDrop);
-            tile.addEventListener("dragend", dragEnd);
-
             document.getElementById("board").append(tile);
             row.push(tile);
         }
@@ -49,132 +37,15 @@ function startGame() {
     }
 }
 
-// Hàm này được gọi khi một ô bắt đầu được kéo
-function dragStart() {
-    currTile = this;
+// Hàm này chọn ngẫu nhiên một loại kẹo từ danh sách candies
+function randomCandy() {
+    return candies[Math.floor(Math.random() * candies.length)];
 }
 
-// Hàm này được gọi khi một ô được kéo qua
-function dragOver(e) {
-    e.preventDefault();
-}
-
-// Hàm này được gọi khi một ô kéo vào
-function dragEnter(e) {
-    e.preventDefault();
-}
-
-// Hàm này được gọi khi một ô kéo rời đi
-function dragLeave() {
-}
-
-// Hàm này được gọi khi một ô được thả vào
-function dragDrop() {
-    otherTile = this;
-}
-
-// Hàm này được gọi khi một ô kéo kết thúc
-function dragEnd() {
-    if (currTile.src.includes("blank") || otherTile.src.includes("blank")) {
-        return;
-    }
-
-    let currCoords = currTile.id.split("-");
-    let r = parseInt(currCoords[0]);
-    let c = parseInt(currCoords[1]);
-
-    let otherCoords = otherTile.id.split("-");
-    let r2 = parseInt(otherCoords[0]);
-    let c2 = parseInt(otherCoords[1]);
-
-    let moveLeft = c2 == c - 1 && r == r2;
-    let moveRight = c2 == c + 1 && r == r2;
-    let moveUp = r2 == r - 1 && c == c2;
-    let moveDown = r2 == r + 1 && c == c2;
-    
-    let isAdjacent = moveLeft || moveRight || moveUp || moveDown;
-
-    if (isAdjacent) {
-        let currImg = currTile.src;
-        let otherImg = otherTile.src;
-        currTile.src = otherImg;
-        otherTile.src = currImg;
-
-        let validMove = checkValid();
-        if (!validMove) {
-            let currImg = currTile.src;
-            let otherImg = otherTile.src;
-            currTile.src = otherImg;
-            otherTile.src = currImg;
-        }
-    }
-}
-
-// Hàm này kiểm tra và xử lý việc "nghiền" kẹo
+// Hàm này xử lý việc "nghiền" kẹo
 function crushCandy() {
     crushThree();
     document.getElementById("score").innerText = score;
-}
-
-// Hàm này kiểm tra và "nghiền" các cụm kẹo có ba phần tử giống nhau
-function crushThree() {
-    // Kiểm tra hàng ngang
-    for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < columns - 2; c++) {
-            let candy1 = board[r][c];
-            let candy2 = board[r][c+1];
-            let candy3 = board[r][c+2];
-            if (candy1.src == candy2.src && candy2.src == candy3.src && !candy1.src.includes("blank")) {
-                candy1.src = "./Image/blank.png";
-                candy2.src = "./Image/blank.png";
-                candy3.src = "./Image/blank.png";
-                score += 20;
-            }
-        }
-    }
-
-    // Kiểm tra cột dọc
-    for (let c = 0; c < columns; c++) {
-        for (let r = 0; r < rows - 2; r++) {
-            let candy1 = board[r][c];
-            let candy2 = board[r+1][c];
-            let candy3 = board[r+2][c];
-            if (candy1.src == candy2.src && candy2.src == candy3.src && !candy1.src.includes("blank")) {
-                candy1.src = "./Image/blank.png";
-                candy2.src = "./Image/blank.png";
-                candy3.src = "./Image/blank.png";
-                score += 20;
-            }
-        }
-    }
-}
-
-// Hàm này kiểm tra xem có nước đi hợp lệ nào không
-function checkValid() {
-    // Kiểm tra hàng ngang
-    for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < columns - 2; c++) {
-            let candy1 = board[r][c];
-            let candy2 = board[r][c+1];
-            let candy3 = board[r][c+2];
-            if (candy1.src == candy2.src && candy2.src == candy3.src && !candy1.src.includes("blank")) {
-                return true;
-            }
-        }
-    }
-
-    // Kiểm tra cột dọc
-    for (let c = 0; c < columns; c++) {
-        for (let r = 0; r < rows - 2; r++) {
-            let candy1 = board[r][c];
-            let candy2 = board[r+1][c];
-            let candy3 = board[r+2][c];
-            if (candy1.src == candy2.src && candy2.src == candy3.src && !candy1.src.includes("blank")) {
-                return true;
-            }
-        }
-    }
-    return false;
 }
 
 // Hàm này xử lý việc trượt các viên kẹo khi có ô trống
@@ -196,17 +67,33 @@ function slideCandy() {
     }
 }
 
-// Hàm này tạo ngẫu nhiên hình ảnh kẹo
-function randomCandy() {
-    return candies[Math.floor(Math.random() * candies.length)]; //0 - 5.99
-}
-
-// Hàm này tạo ngẫu nhiên hình ảnh kẹo
+// Hàm này tạo ngẫu nhiên hình ảnh kẹo ở hàng đầu tiên nếu có ô trống
 function generateCandy() {
     // Tạo kẹo mới ở hàng đầu tiên nếu có ô trống
     for (let c = 0; c < columns; c++) {
         if (board[0][c].src.includes("blank")) {
-            board[0][c].src = "./Image/" + randomCandy() + ".png"; 
+            board[0][c].src = "./Image/" + randomCandy() + ".png";
+        }
+    }
+}
+
+// Hàm này xử lý sự kiện cho cảm ứng và click
+function handleEvents() {
+    if (isTouchDevice()) {
+        // Xử lý sự kiện cảm ứng cho điện thoại
+        for (let r = 0; r < rows; r++) {
+            for (let c = 0; c < columns; c++) {
+                let tile = board[r][c];
+                tile.addEventListener("touchstart", handleTouchStart);
+            }
+        }
+    } else {
+        // Xử lý sự kiện click cho máy tính
+        for (let r = 0; r < rows; r++) {
+            for (let c = 0; c < columns; c++) {
+                let tile = board[r][c];
+                tile.addEventListener("click", handleClick);
+            }
         }
     }
 }
@@ -215,36 +102,6 @@ function generateCandy() {
 function isTouchDevice() {
     return (('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0));
 }
-
-// Hàm này xử lý sự kiện click cho máy tính
-function handleMouseEvents() {
-    for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < columns; c++) {
-            let tile = board[r][c];
-            tile.addEventListener("click", handleClick);
-        }
-    }
-}
-
-// Xử lý sự kiện click cho máy tính
-function handleMouseEvents() {
-    for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < columns; c++) {
-            let tile = board[r][c];
-            tile.addEventListener("click", handleClick);
-        }
-    }
-}
-
-// Xử lý sự kiện cảm ứng cho điện thoại
-function handleTouchEvents() {
-    for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < columns; c++) {
-            let tile = board[r][c];
-            tile.addEventListener("touchstart", handleTouchStart);
-        }
-    }
-} 
 
 // Hàm này được gọi khi một ô được click
 function handleClick() {
@@ -274,4 +131,3 @@ function handleTouchStart(event) {
     // Thực hiện xử lý cho ô được chạm ở đây
     console.log("Touched at row " + row + " and column " + col);
 }
-
